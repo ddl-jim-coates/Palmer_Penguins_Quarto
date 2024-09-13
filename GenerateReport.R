1b7c357d287f045e91999fa13d7badaefd9fa838
Jim Coates <jim.coates@dominodatalab.com>
2024-09-13 04:58:56 -0700

    Pushed from Domino: https://se-demo.domino.tech/workspace/jim_coates/Palmer_Penguins_Quarto_Git?executionId=66e41c52ef55515d253d714e

--- GenerateReport.R
+++
@@ -1,9 +0,0 @@
-library(quarto)
-
-args <- commandArgs(trailingOnly = TRUE)
-a <- args[1]
-b <- args[2]
-
-file_name = paste0(a,Sys.Date(),"_Penguin_report",".pdf")
-
-quarto_render("HelloPdf.qmd", output_file = file_name, output_format = "pdf")
